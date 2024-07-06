81fd8df8e53b33e3f3cf78ee1b1a52194bd93761
Takuya Karato <rabitto48@gmail.com>
2024-07-06 18:00:11 +0900

    実装機能がとりあえず動く

---
+++ keyboards/leap/75/mcuconf.h
@@ -0,0 +1,12 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include_next <mcuconf.h>
+
+#undef RP_PWM_USE_PWM4
+#define RP_PWM_USE_PWM4 TRUE
+
+#undef RP_I2C_USE_I2C1
+#define RP_I2C_USE_I2C1 TRUE
